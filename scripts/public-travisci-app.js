9e7f64c4b2afad5ee58ffae7ae222c6006130226
Ryan Smith <3045513+ryansmith94@users.noreply.github.com>
2017-12-17 19:45:17 +0000

    fix(deps): Uses semantic-release's git plugin for the last tag. (#2)

--- scripts/public-travisci-app.js
+++ scripts/public-travisci-app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const release = require('../utils/release');
 
 release({
-  "getLastRelease": path.join(__dirname, '../plugins/getLastRelease'),
+  "getLastRelease": "@semantic-release/git",
   "verifyConditions": [
     "semantic-release-docker",
     "@semantic-release/condition-travis",
--- scripts/public-travisci-lib.js
+++ scripts/public-travisci-lib.js
@@ -3,7 +3,7 @@ const path = require('path');
 const release = require('../utils/release');
 
 release({
-  "getLastRelease": path.join(__dirname, '../plugins/getLastRelease'),
+  "getLastRelease": "@semantic-release/git",
   "verifyConditions": [
     "@semantic-release/condition-travis",
     "@semantic-release/github",
