69606102e7efe9a51b56893d29d73ee46e773bdd
Ryan Smith <0ryansmith1994@gmail.com>
2018-02-09 17:08:40 +0000

    fix(deps): Updates to semantic-release v13.

--- scripts/private-travisci.js
+++ scripts/private-travisci.js
@@ -3,7 +3,6 @@ const path = require('path');
 const release = require('../utils/release');
 
 release({
-  "getLastRelease": "@semantic-release/git",
   "verifyConditions": [
     "@semantic-release/condition-travis",
     "@semantic-release/github"
