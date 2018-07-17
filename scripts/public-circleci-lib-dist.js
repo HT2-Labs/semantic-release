840c67c6a001e54ca2fafb405d1de25f02a65bcd
Ryan Smith <0ryansmith1994@gmail.com>
2018-07-17 11:55:26 +0100

    fix: Copies assets to fix release from dist.

--- scripts/public-circleci-lib-dist.js
+++ scripts/public-circleci-lib-dist.js
@@ -1,19 +1,33 @@
 #!/usr/bin/env node
 const release = require('../utils/release');
+const fs = require('fs-extra');
 
-release({
-  "verifyConditions": [
-    "condition-circle",
-    "@semantic-release/github",
-    "@semantic-release/npm"
-  ],
-  "prepare": [
-    "@semantic-release/npm"
-  ],
-  "publish": [
-    "@semantic-release/github",
-    "@semantic-release/npm"
-  ],
-  "pkgRoot": "dist",
-  "assets": ["package.json", "license", "readme.md"],
-});
+const main = async () => {
+  try {
+    await Promise.all([
+      fs.copy('./package.json', './dist/package.json'),
+      fs.copy('./readme.md', './dist/readme.md'),
+      fs.copy('./license', './dist/license'),
+    ]);
+    await release({
+      "verifyConditions": [
+        "condition-circle",
+        "@semantic-release/github",
+        "@semantic-release/npm"
+      ],
+      "prepare": [
+        "@semantic-release/npm"
+      ],
+      "publish": [
+        "@semantic-release/github",
+        "@semantic-release/npm"
+      ],
+      "pkgRoot": "dist"
+    });
+  } catch (err) {
+    console.error('Release error', err.message);
+    console.log(err.stack);
+  }
+};
+
+main();
