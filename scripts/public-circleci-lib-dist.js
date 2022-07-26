0ea082ba070314e92ddc7a0d6f51795f100582e6
Christopher McLaughlin <christopher@learningpool.com>
2022-07-26 11:22:34 +0100

    fix(Semantic Release): Prevent overwrite of bundled dist/package.json [CICD-199]

--- scripts/public-circleci-lib-dist.js
+++ scripts/public-circleci-lib-dist.js
@@ -2,13 +2,36 @@
 const release = require('../utils/release');
 const fs = require('fs-extra');
 
+async function getDistFiles() {
+  const distfiles = [];
+
+  // Don't override the dist/package.json if it's already bundled.
+  if (!fs.existsSync('./dist/package.json')) {
+    distfiles.push(fs.copy('./package.json', './dist/package.json'));
+  }
+
+  if (fs.existsSync('./readme.md')) {
+    distfiles.push(fs.copy('./readme.md', './dist/readme.md'));
+  } else if (fs.existsSync('./README.md')) {
+    distfiles.push(fs.copy('./README.md', './dist/README.md'));
+  } else {
+    console.log('No readme/README found.');
+  }
+
+  if (fs.existsSync('./license')) {
+    distfiles.push(fs.copy('./license', './dist/license'));
+  } else if (fs.existsSync('./LICENSE')) {
+    distfiles.push(fs.copy('./LICENSE', './dist/LICENSE'));
+  } else {
+    console.log('No license/LICENSE found.');
+  }
+
+  return distfiles;
+}
+
 const main = async () => {
   try {
-    await Promise.all([
-      fs.copy('./package.json', './dist/package.json'),
-      fs.copy('./readme.md', './dist/readme.md'),
-      fs.copy('./license', './dist/license'),
-    ]);
+    await Promise.all(await getDistFiles());
     await release({
       "verifyConditions": [
         "@semantic-release/github",
