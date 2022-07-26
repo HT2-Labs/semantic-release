194c73c28d17fe548101c335dbc8c19ed650b25b
Christopher McLaughlin <christopher@learningpool.com>
2022-07-26 13:36:36 +0100

    fix(Semantic Release): Prevent overwrite of bundled dist/package.json [CICD-199]

--- scripts/public-circleci-lib-dist.js
+++ scripts/public-circleci-lib-dist.js
@@ -2,36 +2,32 @@
 const release = require('../utils/release');
 const fs = require('fs-extra');
 
-async function getDistFiles() {
-  const distfiles = [];
-
-  // Don't override the dist/package.json if it's already bundled.
-  if (!fs.existsSync('./dist/package.json')) {
-    distfiles.push(fs.copy('./package.json', './dist/package.json'));
+async function copyBestPath(bestPaths, targetPath, overwrite = true) {
+  if (!overwrite
+     && fs.existsSync(targetPath)) {
+    console.log(`Skipping ${bestPaths} as ${targetPath} has targeted file.`);
+    return;
   }
 
-  if (fs.existsSync('./readme.md')) {
-    distfiles.push(fs.copy('./readme.md', './dist/readme.md'));
-  } else if (fs.existsSync('./README.md')) {
-    distfiles.push(fs.copy('./README.md', './dist/README.md'));
-  } else {
-    console.log('No readme/README found.');
+  const bestPath = bestPaths.find((potentialBestPath) => {
+    return fs.existsSync(potentialBestPath);
+  });
+
+  if (bestPath === undefined) {
+    console.log(`Could not find a best path for ${targetPath} from ${bestPaths.join(' ')}`);
+    return;
   }
 
-  if (fs.existsSync('./license')) {
-    distfiles.push(fs.copy('./license', './dist/license'));
-  } else if (fs.existsSync('./LICENSE')) {
-    distfiles.push(fs.copy('./LICENSE', './dist/LICENSE'));
-  } else {
-    console.log('No license/LICENSE found.');
-  }
-
-  return distfiles;
+  return fs.copy(bestPath, targetPath);
 }
 
 const main = async () => {
   try {
-    await Promise.all(await getDistFiles());
+    await Promise.all([
+      copyBestPath(['./package.json'], './dist/package.json', false),
+      copyBestPath(['./readme.md', './README.md'], './dist/README.md'),
+      copyBestPath(['./license', './LICENSE'], './dist/LICENSE'),
+    ]);
     await release({
       "verifyConditions": [
         "@semantic-release/github",
